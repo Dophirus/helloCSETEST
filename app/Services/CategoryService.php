f78adac461edaff30848f386f5a53dde2c3e4c67
Dophirus <tomcollet59@gmail.com>
2026-05-14 12:08:00 +0200

    adding request, resource and service

---
+++ app/Services/CategoryService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Category;
+use Illuminate\Support\Collection;
+
+class CategoryService
+{
+    public function getAllCategories(): Collection
+    {
+        return Category::all();
+    }
+
+    public function createCategory(array $data): Category
+    {
+        return Category::create($data);
+    }
+
+    public function updateCategory(Category $category, array $data): Category
+    {
+        $category->update($data);
+        return $category;
+    }
+
+    public function deleteCategory(Category $category): bool
+    {
+        return $category->delete();
+    }
+}
